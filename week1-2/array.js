77ef035fe5ecebcb31b8b066157dcd2ee1be3abb
Henrik <henrik.norelius@gmail.com>
2021-09-22 14:08:28 +0200

    ok test

---
+++ week1-2/array.js
@@ -0,0 +1,56 @@
+
+names = ["Johan",
+        "Niklas",
+        "Anna",
+        "Philip",
+]
+
+
+/*
+names.array.forEach(element => {
+    
+});
+*/
+
+names.forEach( function(name, index) {
+    console.log(index, name)
+})
+
+/*
+names.forEach( function(element, iterator){
+
+    })
+
+*/
+
+function multiply_by_2(x, index) {
+    console.log(2*x, index)
+}
+
+const ages = [34, 12, 31, 43, 33]
+
+ages.forEach(multiply_by_2)
+
+/*
+array.map()
+*/
+
+const greeting = names.map( function(name) {
+    return `hej ${name}`
+})
+
+console.log(greeting)
+
+
+names.forEach(function(name) {
+    if (name === "Johan")
+})
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+ 
